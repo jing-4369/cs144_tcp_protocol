32a0160da5bdff50d31fcf79b2f624dfb86c3613
zjuJing <1319422661@qq.com>
2022-11-21 20:35:00 +0800

    pass all tests but send_extra

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -32,40 +32,68 @@ TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const s
 uint64_t TCPSender::bytes_in_flight() const { return _bytes_flight; }
 
 void TCPSender::fill_window() {
+	if (_next_seqno == _stream.bytes_written() + 2 ) {
+		return;
+	}
     TCPSegment seg;
-	if (_next_seqno == 0) {
-		seg.header().syn = true;
-		_next_seqno++;
-	}
-	else {
-		size_t seg_size = min(TCPConfig::MAX_PAYLOAD_SIZE, min(_stream.buffer_size(), _window_size));
-		seg.parse(Buffer(_stream.read(seg_size)));
-	}
-	seg.header().seqno = wrap(_next_seqno, _isn);
-    _segments_out.push(seg);
-    _segments_flying.push_back(seg);
-    _bytes_flight += seg.length_in_sequence_space();
-    if (!_timer.is_running()) {
-        _timer.run_timer(_cur_time);
+	size_t seg_size = min(TCPConfig::MAX_PAYLOAD_SIZE, min(_stream.buffer_size(), _window_size));
+	if (seg_size || _next_seqno == 0 || (_stream.input_ended() && _window_size)) {
+		seg.header().seqno = wrap(_next_seqno, _isn);
+		if (_stream.input_ended()) {
+			seg.header().fin = true;
+			if (seg_size) {
+				seg.payload() = _stream.read(seg_size); 
+				_next_seqno += seg.length_in_sequence_space();
+				_window_size -= seg.length_in_sequence_space();
+			}
+			else {
+				_next_seqno++;
+			}
+		}
+		else if (seg_size) {
+			seg.payload() = _stream.read(seg_size); 
+			_next_seqno += seg.length_in_sequence_space();
+			_window_size -= seg.length_in_sequence_space();
+		}
+		else {
+			seg.header().syn = true;
+			_next_seqno++;
+		}
+		_segments_out.push(seg);
+		_segments_flying.push_back(seg);
+		_bytes_flight += seg.length_in_sequence_space();
+		if (!_timer.is_running()) {
+			_timer.run_timer(_cur_time);
+		}
     }
+	else if (_stream.input_ended() && _window_size) {
+		
+	}
 }
 
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
 void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
     uint64_t abs_ackno = unwrap(ackno, _isn, next_seqno_absolute());
+	if (abs_ackno == 0 || abs_ackno > _next_seqno) {
+		return;
+	}
     while (!_segments_flying.empty()) {
         WrappingInt32 cur_no =
             _segments_flying.front().header().seqno + _segments_flying.front().length_in_sequence_space();
         uint64_t abs_curno = unwrap(cur_no, _isn, next_seqno_absolute());
-        if (abs_ackno > abs_curno) {
+        if (abs_ackno >= abs_curno) {
             _bytes_flight -= _segments_flying.front().length_in_sequence_space();
             _segments_flying.pop_front();
+			_timer.reset_timeout(_initial_retransmission_timeout);
+			_timer.run_timer(_cur_time);
         }
+		else {
+			break;
+		}
     }
-    _next_seqno = abs_ackno;
+    _next_seqno = max(_next_seqno, abs_ackno);
     _window_size = window_size;
-    fill_window();
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
@@ -74,7 +102,7 @@ void TCPSender::tick(const size_t ms_since_last_tick) {
     if (_timer.is_running() && _timer.is_expired(ms_since_last_tick)) {
         _timer.double_timeout();
         _timer.run_timer(_cur_time);
-        consecutive_retransmissions();
+		_segments_out.push(_segments_flying.front());
     }
 }
 
